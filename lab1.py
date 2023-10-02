7e9cf2e32c9dfa7517aa2a004285e3cccc7a2a29
gapatun337 <144454364+gapatun337@users.noreply.github.com>
2023-10-02 21:25:52 +0500

    Add files via upload

--- lab1.py
+++ lab1.py
@@ -30,3 +30,59 @@ def task2():
 
 
 task2()
+
+import math
+
+
+def task3():
+    print('Привет! Приступаю к решению задания N3')
+
+    x = float(input('введи x: '))
+
+    base = 3
+
+    x_result = (math.log(math.cosh(x ** 1 / 3), base))
+
+    print('Ответ: x = {:.4f}. Спасибо, что воспользовался мной!'.format(x_result))
+
+
+task3()
+
+import math
+
+
+def task5():
+    print('Привет! Приступаю к решению задания N5')
+
+    m1 = float(input('введи m1: '))
+    v1 = float(input('введи v1: '))
+    m2 = float(input('введи m2: '))
+    v2 = float(input('введи v2: '))
+
+    v = (m1 * v1 + m2 * v2) / (m1 + m2)
+    v1_after = 2 * v - v1
+    v2_after = 2 * v - v2
+
+    print("Скорость движения тележек после соударения: {:.2f} м/с".format(v))
+    print("Скорость первой тележки после соударения: {:.2f} м/с".format(v1_after))
+    print("Скорость второй тележки после соударения: {:.2f} м/с".format(v2_after))
+
+
+task5()
+
+
+def task9():
+    print('Привет! Приступаю к решению задания N9')
+
+    exchange_rate = 0.098
+    commission = 0.01
+
+    amount_yuan = float(input("Введите сумму в юанях ¥: "))
+
+    amount_rubles = amount_yuan * exchange_rate * (1 - commission)
+    amount_rubles = round(amount_rubles, 2)
+
+    print("Сумма в рублях: {:.2f}".format(amount_rubles))
+
+
+task9()
